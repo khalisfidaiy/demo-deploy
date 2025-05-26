f7c8b78627ff8d4dd439ea291baf5e152c66d6ab
khalisfidaiy <80872128+khalisfidaiy@users.noreply.github.com>
2025-05-26 16:41:41 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -11,15 +11,12 @@ app = Flask(__name__)
 # Define a route for prediction 
 @app.route("/predict", methods=["POST"]) 
 def predict(): 
-    try: 
-        data = request.json  # Receive JSON input 
-        image = np.array(data["image"]).reshape(1, 28, 28) / 255.0  # Preprocess input 
-        prediction = model.predict(image) 
-        response = {"prediction": int(np.argmax(prediction))}  # Return highest probability digit 
-        return jsonify(response) 
-    except Exception as e: 
-        return jsonify({"error": str(e)}) 
- 
-# Run Flask application 
+    data = request.json(force=True)  # Receive JSON input 
+    image = np.array(data["instances"])  # Preprocess input 
+    prediction = model.predict(image)
+    return jsonify(predictions.tolist())
+
+@app.route("/health", method)
+
 if __name__ == "__main__": 
     app.run(debug=True)
